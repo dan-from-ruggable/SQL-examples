9fd9dc612f415d04fadc125557f46f5750e89a2f
dan-kwon <danielkwon02@gmail.com>
2021-08-16 14:02:25 -0700

    added daily sales sql

--- sql/daily_sales_by_function-texture-size.sql
+++ sql/daily_sales_by_function-texture-size.sql
@@ -1,6 +1,11 @@
-SELECT *
+SELECT o.created_date,
+       o.function,
+       o.texture,
+       o.size,
+       SUM(o.rug_quantity) AS quantity
 FROM shopify_ruggable.mv_ruggable_shopify_orders_detailed o
 WHERE disc_equals_gross = 'include'
 AND o.product_type IN ('Rug','Doormat')
 AND customer_email IS NOT NULL
-LIMIT 25
+GROUP BY 1,2,3,4
+ORDER BY 1,2,3,4
