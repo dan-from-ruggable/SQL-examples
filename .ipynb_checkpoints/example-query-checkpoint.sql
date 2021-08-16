2388d1c148cd3528cfd6f2eba33e1826e7cd4f53
dan-kwon <danielkwon02@gmail.com>
2021-08-16 13:38:55 -0700

    removed ipynb checkpoints

--- .ipynb_checkpoints/example-query-checkpoint.sql
+++
@@ -1,6 +0,0 @@
-SELECT *
-FROM shopify_ruggable.mv_ruggable_shopify_orders_detailed o
-WHERE disc_equals_gross = 'include'
-AND o.product_type IN ('Rug','Doormat')
-AND customer_email IS NOT NULL
-LIMIT 25
